2352a8fecca39638b2d307f4d73b906da19255b4
Leonardo. <leo.leite@hotmail.com>
2024-05-04 00:41:20 -0300

    init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+import withSerwistInit from "@serwist/next";
 const nextConfig = {};
 
-export default nextConfig;
+const withSerwist = withSerwistInit({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
+
+export default withSerwist({
+    nextConfig
+});
