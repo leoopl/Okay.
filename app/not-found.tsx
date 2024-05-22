b2b3e133cb5c3449a7c71d013dda055f7791443b
Leonardo. <leo.leite@hotmail.com>
2024-05-22 11:11:02 -0300

    eslint + prettier

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,20 +1,32 @@
-import Image from 'next/image';
-import Link from 'next/link';
+import Image from "next/image";
+import Link from "next/link";
 
 function randomInt(max: number) {
-    return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 export default function NotFound() {
-    return (
-        <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
-            <div className="text-center">
-                <Image width={550} height={440} alt="404" src={`/404${randomInt(4)}.svg`} />
-                <p className="mt-6 text-base font-bold leading-7 text-gray-900">Algo inesperado aconteceu, não achamos a página desejada!</p>
-                <div className="mt-10 flex items-center justify-center gap-x-6">
-                    <Link href="/" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Vamos voltar e tentar de novo?</Link>
-                </div>
-            </div>
-        </main>
-    );
+  return (
+    <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div className="text-center">
+        <Image
+          width={550}
+          height={440}
+          alt="404"
+          src={`/404${randomInt(4)}.svg`}
+        />
+        <p className="mt-6 text-base font-bold leading-7 text-gray-900">
+          Algo inesperado aconteceu, não achamos a página desejada!
+        </p>
+        <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link
+            href="/"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Vamos voltar e tentar de novo?
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }
--- app/singup/page.tsx
+++ app/singup/page.tsx
@@ -1,138 +1,140 @@
 import Image from "next/image";
 import Link from "next/link";
 
-
 export default function Longin() {
-    return (
-      <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <Image alt="experience" width={510} height={450} src="/singup.svg" />
-            <h2 className="text-center text-4xl font-bold leading-9 tracking-tight text-gray-900 small-caps">
-              Faça a sua conta!
-            </h2>
-          </div>
-  
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-4" action="#" method="POST">
-              <div>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    required
-                    placeholder="Nome"
-                    className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="mt-2">
-                  <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
-                    required
-                    placeholder="Sobrenome"
-                    className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="mt-2">
-                  <input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    placeholder="E-mail"
-                    className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    placeholder="Senha"
-                    className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="mt-2">
-                  <input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    placeholder="Repita a senha"
-                    className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <div className="mt-2">
-                  <input
-                    id="birthday"
-                    name="birthday"
-                    type="date"
-                    required
-                    placeholder="Data de Nascimento"
-                    className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-3">
-                <div className="mt-2">
-                    <select
-                        id="gender"
-                        name="gender"
-                        required
-                        defaultValue={"Gênero"}
-                        className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    >
-                        <option disabled>Gênero</option>
-                        <option>Mulher</option>
-                        <option>Homem</option>
-                        <option>LGBTQIA+</option>
-                    </select>
-                </div>
-            </div>
-  
-              <div>
-                <button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Cadastre-se
-                </button>
-              </div>
-            </form>
-  
-            <p className="mt-2 text-center text-sm text-gray-900 small-caps">
-              Já tem conta?{' '}
-              <Link href="/singin" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-                Entrar
-              </Link>
-            </p>
-          </div>
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Image alt="experience" width={510} height={450} src="/singup.svg" />
+          <h2 className="text-center text-4xl font-bold leading-9 tracking-tight text-gray-900 small-caps">
+            Faça a sua conta!
+          </h2>
         </div>
-      </>
-    )
-  }
+
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-4" action="#" method="POST">
+            <div>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  required
+                  placeholder="Nome"
+                  className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  required
+                  placeholder="Sobrenome"
+                  className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  placeholder="E-mail"
+                  className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  placeholder="Senha"
+                  className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  placeholder="Repita a senha"
+                  className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="mt-2">
+                <input
+                  id="birthday"
+                  name="birthday"
+                  type="date"
+                  required
+                  placeholder="Data de Nascimento"
+                  className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <div className="mt-2">
+                <select
+                  id="gender"
+                  name="gender"
+                  required
+                  defaultValue={"Gênero"}
+                  className="block w-full rounded-md border-0 py-1.5 bg-transparent text-gray-900 shadow-sm ring-1 ring-inset ring-gray-500 placeholder:text-gray-600 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                >
+                  <option disabled>Gênero</option>
+                  <option>Mulher</option>
+                  <option>Homem</option>
+                  <option>LGBTQIA+</option>
+                </select>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Cadastre-se
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-2 text-center text-sm text-gray-900 small-caps">
+            Já tem conta?{" "}
+            <Link
+              href="/singin"
+              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+            >
+              Entrar
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
