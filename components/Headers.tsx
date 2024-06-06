20ca87e5afc4c1b586a92278a4970b799e7de061
Wagner Aladiah Pacheco de Almeida <wagner.almeida@develoci.com.br>
2024-06-05 22:50:26 -0300

    WIP - Update navbar appearance on desktop

--- components/Headers.tsx
+++ components/Headers.tsx
@@ -2,29 +2,43 @@ import Link from "next/link";
 import Image from "next/image";
 
 export default function Header() {
-    const pages = [
-		{ name: 'Recursos e informações', link: 'information' },
-		{ name: 'Técnica de respiração', link: 'breath' },
-		{ name: 'Ajuda profissional', link: 'professionalhelp' },
-		{ name: 'Chat de suporte', link: 'chatcvv' },
-		{ name: 'Como está se sentindo?', link: 'questionnaire' },
-	];
+  const pages = [
+    { name: "Recursos e informações", link: "information" },
+    { name: "Técnica de respiração", link: "breath" },
+    { name: "Ajuda profissional", link: "professionalhelp" },
+    { name: "Chat de suporte", link: "chatcvv" },
+    { name: "Como está se sentindo?", link: "questionnaire" },
+  ];
 
-    return(
-        <div className="container mx-auto flex items-center border-b-2 px-6 py-2 h-24">
-            {/* <Image src="/logo.svg" alt="Okay!" width={25} height={25} /> */}
-            <Link href={'/'} className="font-bold text-selectiveyellow">Okay!</Link>
-            <div className="grow">
-                <div className="flex items-center justify-center gap-2 md:gap-8">
-                    {pages.map((page) => (
-                        <Link href={`/${page.link}`} key={page.link} className="text-selectiveyellow">{page.name}</Link>
-                    ))}
-                </div>
-            </div>
-            <div>
-                <Link href="/singup" className="mr-2 font-bold text-selectiveyellow">Sing Up</Link>
-                <Link href="/singin" className="font-bold text-selectiveyellow">Login</Link>
-            </div>
+  return (
+    <div className="w-full bg-amber-50 font-bold text-black text-base drop-shadow">
+      <div className="justify-between flex items-center h-12 w-4/5 mx-auto">
+        <Link href={"/"} className="text-xl">
+          Okay!
+        </Link>
+        <div>
+          <Link href="/signup" className="mr-4">
+            Sign Up
+          </Link>
+          <Link href="/signin">Login</Link>
         </div>
-    )
-};
+      </div>
+      <div className="mx-auto flex items-center h-12 w-4/5 font-normal">
+        {/* <Image src="/logo.svg" alt="Okay!" width={25} height={25} /> */}
+        <div className="grow">
+          <div className="flex items-center justify-between gap-2 md:gap-8">
+            {pages.map((page) => (
+              <Link
+                href={`/${page.link}`}
+                key={page.link}
+                className="text-black text-sm"
+              >
+                {page.name}
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
