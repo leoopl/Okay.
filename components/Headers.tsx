0510617aa28d22be674da0d7bc62c8379d2961ef
Wagner Aladiah Pacheco de Almeida <wagner.aladiah@gmail.com>
2024-06-12 09:21:16 -0300

    WIP - Implement mobile menu functionality

--- components/Headers.tsx
+++ components/Headers.tsx
@@ -1,7 +1,13 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+
   const pages = [
     { name: "Recursos e informações", link: "information" },
     { name: "Técnica de respiração", link: "breath" },
@@ -11,12 +17,17 @@ export default function Header() {
   ];
 
   return (
-    <div className="w-full bg-amber-50 font-bold text-black text-base drop-shadow">
+    <div className={`w-full font-bold text-black text-base drop-shadow fixed z-10 transition-colors duration-300 ${isScrolled ? 'bg-amber-50' : ''}`}>
       <div className="justify-between flex items-center h-12 w-4/5 mx-auto">
+        <button onClick={() => setIsOpen(!isOpen)} className="lg:hidden">
+          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
         <Link href={"/"} className="text-xl">
           Okay!
         </Link>
-        <div>
+        <div className="hidden lg:flex">
           <Link href="/signup" className="mr-4">
             Sign Up
           </Link>
@@ -26,7 +37,7 @@ export default function Header() {
       <div className="mx-auto flex items-center h-12 w-4/5 font-normal">
         {/* <Image src="/logo.svg" alt="Okay!" width={25} height={25} /> */}
         <div className="grow">
-          <div className="flex items-center justify-between gap-2 md:gap-8">
+          <div className={`mx-auto flex items-center h-12 font-normal ${isOpen ? 'flex' : 'hidden'} lg:flex`}>
             {pages.map((page) => (
               <Link
                 href={`/${page.link}`}
