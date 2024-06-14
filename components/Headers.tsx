d8e676daa9a88b06c9072163b66377957fc9fdf7
Wagner Aladiah Pacheco de Almeida <wagner.aladiah@gmail.com>
2024-06-14 07:39:18 -0300

    Style mobile header

--- components/Headers.tsx
+++ components/Headers.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
 import { useState } from "react";
 
 export default function Header() {
@@ -17,11 +16,26 @@ export default function Header() {
   ];
 
   return (
-    <div className={`w-full font-bold text-black text-base drop-shadow fixed z-10 transition-colors duration-300 ${isScrolled ? 'bg-amber-50' : ''}`}>
-      <div className="justify-between flex items-center h-12 w-4/5 mx-auto">
+    <div
+      className={`w-full font-bold text-black text-base drop-shadow fixed z-10 transition-colors duration-300 ${
+        isScrolled ? "bg-amber-50" : ""
+      }`}
+    >
+      <div className="justify-between flex items-center w-4/5 mx-auto h-12">
         <button onClick={() => setIsOpen(!isOpen)} className="lg:hidden">
-          <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
           </svg>
         </button>
         <Link href={"/"} className="text-xl">
@@ -34,15 +48,19 @@ export default function Header() {
           <Link href="/signin">Login</Link>
         </div>
       </div>
-      <div className="mx-auto flex items-center h-12 w-4/5 font-normal">
+      <div className="mx-auto flex items-center w-4/5 font-normal h-12">
         {/* <Image src="/logo.svg" alt="Okay!" width={25} height={25} /> */}
         <div className="grow">
-          <div className={`mx-auto flex items-center h-12 font-normal ${isOpen ? 'flex' : 'hidden'} lg:flex`}>
+          <div
+            className={`mx-auto flex justify-between items-center font-normal ${
+              isOpen ? "flex" : "hidden"
+            } lg:flex lg:h-12 max-lg:flex-col max-lg:bg-white max-lg:absolute max-lg:items-start max-lg:p-5 max-lg:left-1/5 max-lg:top-12 max-lg:h-fit max-lg:gap-6`}
+          >
             {pages.map((page) => (
               <Link
                 href={`/${page.link}`}
                 key={page.link}
-                className="text-black text-sm"
+                className="text-black text-sm pb-2"
               >
                 {page.name}
               </Link>
